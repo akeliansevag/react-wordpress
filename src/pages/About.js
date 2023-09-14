551dddba5d4a6e07d8f79e6c98974f9db07c2380
akeliansevag <akelian.sevag@gmail.com>
2023-09-14 16:28:32 +0300

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import { fetchPage } from '../api/wordpress';
 const About = () => {
-  const [page, setPage] = useState({});
+  const [page, setPage] = useState([]);
   
   useEffect(()=>{
     fetchPage('about-us').then((res) => setPage(res)).catch((error)=> console.log(error));
@@ -10,16 +10,19 @@ const About = () => {
   
   if(page.length > 0){
     console.log(page);
+  
+    return (
+      <div className='container'>
+        <h1>{page[0].title.rendered}</h1>
+        <img alt='About us' src={page[0].acf.about_us_image.sizes.medium} />
+        <div className='mt-4' dangerouslySetInnerHTML={{ __html: page[0].acf.about_us_description }} />
+      </div>
+    )
+  }else{
+    return (
+      <></>
+    )
   }
- 
-  return (
-    <div className='container'>
-      <h1>{page[0].title.rendered}</h1>
-      <img alt='About us image' src={page[0].acf.about_us_image.sizes.medium} />
-      <div className='mt-4' dangerouslySetInnerHTML={{ __html: page[0].acf.about_us_description }} />
-    </div>
-    
-  )
 }
 
 export default About;
